3493ed066d8c385a95965a7bf2bcca8fd393e05a
Septentrion50 <erwan56lecorre@gmail.com>
2021-07-13 11:27:52 +0200

    Add list file and modify script 1

---
+++ script_1.js
@@ -0,0 +1,30 @@
+// Bubble sorting
+
+class Sorter {
+    constructor(array) {
+        this.array = array;
+        this.count = 0;
+        this.sortMethod();
+    }
+
+    sortMethod() {
+        let isSorted;
+        for (let i = 0; i < this.array.length; i++) {
+            isSorted = true;
+            for (let j = 1; j < this.array.length; j++) {
+                this.count += 1;
+                if(this.array[j] < this.array[j - 1]){
+                    let tmp = this.array[j];
+                    this.array[j] = this.array[j -1];
+                    this.array[j -1] = tmp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted){
+                return;
+            }
+        }
+    }
+}
+
+module.exports = Sorter;
